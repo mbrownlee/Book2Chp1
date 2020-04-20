21dc8a3cd5a32f9f97794ef836f6604185dd65a9
mbrownlee <michelle@orbwave.com>
2020-04-20 16:39:05 -0500

    book2chp1 exercise

---
+++ main.js
@@ -0,0 +1,12 @@
+const head = document.querySelector(".article_header")
+const headers = document.querySelectorAll(".article_header")
+article_header.classlist.remove("article_header")
+article_header.classlist.add("article_header_important")
+
+dashed.classlist.remove("dashed")
+article_footer.classlist.add(".goldenrod")
+
+
+
+
+
